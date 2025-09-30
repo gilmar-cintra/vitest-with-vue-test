72410fbf16c8d88eb286a83ef67fff88b0b82d9b
Gilmar Cintra <gilmarcintra@gmail.com>
2025-09-30 15:53:06 -0300

    Melhorando o teste

--- src/components/Logo.test.js
+++ src/components/Logo.test.js
@@ -4,6 +4,13 @@ import Logo from "./Logo.vue";
 import { mount } from '@vue/test-utils'
 
 test("Deve renderizar o componente Logo", () => {
+
+  //Monto o componente
   const wrapper = mount(Logo);
-  expect(wrapper.text()).toContain("Casa da Salada");
+
+  //Busco o elemento p e pego o texto (busco e pego o que quero)
+  const retorno = wrapper.find("p").text();
+ 
+  //Faço a asserção
+  expect(retorno).toBe("Casa da Salada");
 });
